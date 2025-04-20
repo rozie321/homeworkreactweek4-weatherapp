f5e55a90d266ca643dbfa254de605501c3a6dc32
rozie321 <107440478+rozie321@users.noreply.github.com>
2025-04-20 21:14:35 +0100

    Update Search cities

--- src/Searchcities.js
+++ src/Searchcities.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Searchcities() {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px 0' }}>
-            <input type="text" placeholder="Enter city name" style={{ margin: '10px 0', width: '100%' }} />
-            <button style={{ color: 'green', margin: '10px 0', width: '100%' }}>Search</button>
-            <button style={{ color: 'blue', margin: '10px 0', width: '100%' }}>Current</button>
+            <input type="text" placeholder="Enter city name" style={{ width: '60%' }} />
+            <button style={{ color: 'green',width: '20%' }}>Search</button>
+            <button style={{ color: 'blue', width: '20%' }}>Current</button>
         </div>
     );
 }
