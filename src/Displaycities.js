2493a2652a57dcf20664cb97175178d46392fc8b
rozie321 <107440478+rozie321@users.noreply.github.com>
2025-04-20 21:10:15 +0100

    adding hypelink to the cities

--- src/Displaycities.js
+++ src/Displaycities.js
@@ -4,11 +4,11 @@ import './index.css';
 export default function Displaycities()
 {
     return (
-        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-            <div>Lisbon</div>
-            <div>Paris</div>
-            <div>Sydney</div>
-            <div>San Francisco</div>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly', paddingBottom: '20px' }}>
+            <a href="https://en.wikipedia.org/wiki/Lisbon" target="_blank" rel="noopener noreferrer">Lisbon</a>
+            <a href="https://en.wikipedia.org/wiki/Paris" target="_blank" rel="noopener noreferrer">Paris</a>
+            <a href="https://en.wikipedia.org/wiki/Sydney" target="_blank" rel="noopener noreferrer">Sydney</a>
+            <a href="https://en.wikipedia.org/wiki/San_Francisco" target="_blank" rel="noopener noreferrer">San Francisco</a>
         </div>
-    );
+        );
 }
--- src/Searchcities.js
+++ src/Searchcities.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 export default function Searchcities() {
     return (
-        <div>
-           
-    
-           
-            <input type="text" placeholder="Enter city name" />
-            <button>Search</button>
-            <button>Current</button>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px 0' }}>
+            <input type="text" placeholder="Enter city name" style={{ margin: '10px 0', width: '100%' }} />
+            <button style={{ color: 'green', margin: '10px 0', width: '100%' }}>Search</button>
+            <button style={{ color: 'blue', margin: '10px 0', width: '100%' }}>Current</button>
         </div>
     );
 }
