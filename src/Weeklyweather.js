4a8f8175ad7b822aded286f7d894c9b7f6a6ef25
rozie321 <107440478+rozie321@users.noreply.github.com>
2025-04-20 20:51:48 +0100

    css adjustment

---
+++ src/Weeklyweather.js
@@ -0,0 +1,19 @@
+import React from 'react';
+export default function Weeklyweather() {
+    return (
+        <div>
+            <h1>Weekly Weather</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Monday<br />15°C 🌥</td>
+                        <td>Tuesday<br />15°C 🌥</td>
+                        <td>Wednesday<br />15°C 🌥</td>
+                        <td>Thursday<br />15°C 🌥</td>
+                        <td>Friday<br />15°C 🌥</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import Weeklyweather from './Weeklyweather';
 import Displaycities from './Displaycities';
 import Searchcities from './Searchcities';
 import CurrentWeather from './CurrentWeather';
@@ -9,10 +9,12 @@ import CurrentWeather from './CurrentWeather';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Displaycities />
-    <Searchcities />
-  
-  <CurrentWeather />
+    <div className='container'>
+        <Displaycities />
+        <Searchcities />
+        <CurrentWeather />
+        <Weeklyweather />
+    </div>
   </React.StrictMode>
 );
 
